543cf2dd3ce847f54fbf647f991c8b2f07571097
Matheus P. Silva <matheus.ps@icloud.com>
2018-09-20 23:24:44 -0300

    Added ignore for macOS temp files, created a new project ::: creditcard-form

---
+++ creditcard-form/README.md
@@ -0,0 +1,28 @@
+# Credit Cart Form
+    [TODO]
+
+### Dependencies:
+* [ReactJs] - HTML enhanced for web apps!
+* [Webpack 4] - Module Bundler
+* [Babel] - Javascript compiler
+* [Sass] - CSS with superpowers
+
+##### Build
+```sh
+$ yarn
+$ yarn build
+```
+
+##### Development
+```sh
+$ yarn start
+```
+
+**use it, share it, love it.✌🏾**
+
+[//]: # (Reference links)
+   [matheus]: <https://github.com/matheusps>
+   [ReactJs]: <https://reactjs.org/>
+   [Sass]: <http://sass-lang.com/>
+   [Babel]: <https://babeljs.io/>
+   [Webpack 4]: <https://webpack.js.org/>
---
+++ creditcard-form/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          },
+          {
+              loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
